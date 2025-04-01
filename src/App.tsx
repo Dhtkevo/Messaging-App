7fcdb64d66f74ba65bc839a9c6b9b187500e925c
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-01 16:46:44 -0400

    Create the signup page interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import LoginPage from "../components/LoginPage/LoginPage";
+import RegisterPage from "../components/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <RegisterPage />
     </>
   );
 }
