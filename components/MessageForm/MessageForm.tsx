cec9a03e588cb2f123ea08b3a72457d888e1e672
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-02 13:44:55 -0400

    Create form for sending messages between users

---
+++ components/MessageForm/MessageForm.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function MessageForm() {
+  return (
+    <div className="h-screen bg-amber-100 flex justify-center items-center">
+      <form
+        method="post"
+        action={""}
+        className="border w-1/2 h-1/2 rounded-3xl bg-amber-200 shadow-xl shadow-amber-200 p-4 flex flex-col gap-10"
+      >
+        <h1 className="text-center text-3xl font-bold">Send a Message</h1>
+        <div className="self-center">
+          <label htmlFor="targetUser" className="text-lg">
+            Send To User:{" "}
+          </label>
+          <input
+            name="targetUser"
+            required
+            minLength={4}
+            className="bg-white py-1 px-2 rounded-2xl"
+          />
+        </div>
+        <div className="flex flex-col">
+          <textarea
+            name="messageText"
+            autoComplete="on"
+            maxLength={300}
+            minLength={5}
+            placeholder="Enter your message here..."
+            rows={5}
+            required
+            className="bg-white rounded-2xl p-1 resize-none"
+          />
+        </div>
+        <button className="text-lg bg-green-300 rounded-2xl border hover:bg-green-600 hover:cursor-pointer hover:text-white">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default MessageForm;
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import LoginPage from "../components/LoginPage/LoginPage";
 import RegisterPage from "../components/RegisterPage/RegisterPage";
 import Dashboard from "../components/Dashboard/Dashboard";
 import Profile from "../components/Profile/Profile";
+import MessageForm from "../components/MessageForm/MessageForm";
 
 function App() {
   return (
     <>
-      <Profile />
+      <MessageForm />
     </>
   );
 }
