ca787b06de2602d88113443a26a1540a71cbb349
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-04 17:08:15 -0400

    Update schema to accurately have 1to1 relationship with message and user

--- components/RegisterPage/RegisterPage.tsx
+++ components/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:3000/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.status === 200) {
+      navigate("/login");
+    }
+  };
+
   return (
     <div className="h-screen flex flex-col gap-8 justify-center items-center bg-amber-100">
       <form
@@ -19,6 +40,8 @@ function RegisterPage() {
             minLength={4}
             required
             className="border border-black rounded-full p-2 bg-white"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="flex justify-center items-center gap-2">
@@ -30,9 +53,14 @@ function RegisterPage() {
             name="password"
             required
             className="border border-black rounded-full p-2 bg-white"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button className="border border-black w-1/2 mt-4 h-8 rounded-full hover:bg-amber-300 hover:cursor-pointer">
+        <button
+          onClick={handleSubmit}
+          className="border border-black w-1/2 mt-4 h-8 rounded-full hover:bg-amber-300 hover:cursor-pointer"
+        >
           Register
         </button>
       </form>
