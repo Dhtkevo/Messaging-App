0859129b25f15469f5f3fdc665c75a8a9997ad8f
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-04-05 09:21:19 -0400

    Allow user to update username from profile page

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -3,9 +3,26 @@ import { Link, useNavigate } from "react-router";
 import { UserContext } from "../../context/UserContext";
 
 function Profile() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
   const [messages, setMessages] = useState([]);
+  const [newUsername, setNewUsername] = useState("");
+
+  const handleChangeUsername = async (
+    e: React.FormEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:3000/users/" + user?.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newUsername, userId: user?.id }),
+    });
+
+    setUser({ ...user!, username: newUsername });
+  };
 
   useEffect(() => {
     if (!user) {
@@ -35,9 +52,14 @@ function Profile() {
           name="newUsername"
           className="bg-white p-2 rounded-2xl border"
           minLength={4}
+          value={newUsername}
+          onChange={(e) => setNewUsername(e.target.value)}
           required
         />
-        <button className="bg-fuchsia-300 py-1 px-2 rounded-3xl text-lg hover:bg-fuchsia-400 hover:cursor-pointer">
+        <button
+          onClick={handleChangeUsername}
+          className="bg-fuchsia-300 py-1 px-2 rounded-3xl text-lg hover:bg-fuchsia-400 hover:cursor-pointer"
+        >
           Change Username
         </button>
       </form>
